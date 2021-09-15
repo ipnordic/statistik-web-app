2dbb83af8cb9750fe9c261ae64355250431692fd
mathzb <mathias.baungaard@gmail.com>
2021-09-15 21:31:51 +0200

    updated to work with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import "./App.css";
+import Header from "./components/Header";
 import Agent from "./components/Queue/Agent";
 import Period from "./components/Queue/Period";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className={styles.Container}>
+      <Header />
       <h1>ipnordic Statistik</h1>
-
-      <Period />
-      {/* <Agent /> */}
+      <Period company="2776" queue="" />
+      {/* <Agent company="2776" queue="1530" /> */}
     </div>
   );
 }
