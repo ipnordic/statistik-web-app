3276a46a3056e0fd2a584da79380290666056e9d
mathzb <mathias.baungaard@gmail.com>
2021-09-16 22:21:06 +0200

    update(Agent.jsx) changed table header and props

--- src/components/Queue/Agent.jsx
+++ src/components/Queue/Agent.jsx
@@ -1,35 +1,48 @@
 import useFetch from "../../hooks/useFetch";
 import styles from "../Table.module.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const USERNAME = process.env.REACT_APP_API_USERNAME;
-const PASSWORD = process.env.REACT_APP_API_PASSWORD;
+const Agent = ({ company, queue, startDate, endDate }) => {
+  const { data, loading, error } = useFetch(
+    "Agent",
+    startDate,
+    endDate,
+    company,
+    queue
+  );
 
-const Agent = ({ company = "2776", queue }) => {
-  const url = `https://api-prod01.ipnordic.dk/api/Statistics/Queue/v2/Agent?startDate=2021-09-15&endDate=2021-09-16&company=${company}&queue=${queue}`;
-
-  const { data, loading, error } = useFetch(url, USERNAME, PASSWORD);
   return (
     <>
       {loading && <p>{loading}</p>}
       {error && <p>{error}</p>}
       {data && (
-        <table className={styles.table}>
-          <tbody>
-            <tr className={styles.tableHover}>
-              <th>Navn</th>
-              <th>Lokalnummer</th>
-              <th>Kald besvaret</th>
-            </tr>
-            {data &&
-              data.map((item) => (
-                <tr className={styles.tableHover} key={item.Agent}>
-                  <td className={styles.tdCenter}>{item.Name}</td>
-                  <td className={styles.tdCenter}>{item.Agent}</td>
-                  <td className={styles.tdCenter}>{item.Calls}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <>
+          <table className={styles.table}>
+            <tbody>
+              <tr className={styles.tableHeader}>
+                <th>Navn</th>
+                <th>Lokalnummer</th>
+                <th>Kald Besvaret</th>
+                <th>Gns. Samtaletid</th>
+                <th>Kald Omstillet</th>
+                <th>DND Tid (dagligt)</th>
+                <th>Pause Tid (dagligt)</th>
+              </tr>
+              {data &&
+                data.map((item) => (
+                  <tr className={styles.tableHover} key={item.Agent}>
+                    <td className={styles.tdCenter}>{item.Name}</td>
+                    <td className={styles.tdCenter}>{item.Agent}</td>
+                    <td className={styles.tdCenter}>{item.Calls}</td>
+                    <td className={styles.tdCenter}>{item.AverageCalltime}</td>
+                    <td className={styles.tdCenter}>{item.Transfers}</td>
+                    <td className={styles.tdCenter}>{item.DND}</td>
+                    <td className={styles.tdCenter}>{item.Pause}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
       )}
     </>
   );
