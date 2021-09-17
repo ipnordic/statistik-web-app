6c8f810f088a7306c5e65a2e2742f0b577e97abb
mathzb <mathias.jacobsen@ipnordic.dk>
2021-09-17 14:06:46 +0200

    update(App.jsx) cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import Agent from "./components/Queue/Agent";
-import Period from "./components/Queue/Period";
 import styles from "./App.module.css";
 
 function App() {
@@ -8,19 +7,7 @@ function App() {
     <div className={styles.Container}>
       <Header />
       <h1>ipnordic Statistik</h1>
-      <Period
-        statType="Period"
-        company="4629"
-        queue="1500"
-        startDate="2021-09-16"
-        endDate="2021-09-17"
-      />
-      {/* <Agent
-        company="4629"
-        queue="1500"
-        startDate="2021-09-16"
-        endDate="2021-09-17"
-      /> */}
+      <Agent />
     </div>
   );
 }
