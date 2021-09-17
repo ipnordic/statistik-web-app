ce42fbb6bd73004c3ac5a5655d452cffd8af964b
mathzb <Mathias.baungaard@gmail.com>
2021-09-18 01:31:54 +0200

    update(Header.jsx) added new Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
 import logo from "../assets/logo.png";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <header className={styles.Header}>
-      <a href="http://localhost:3000">
-        <img className={styles.Image} src={logo} alt="logo" />
-      </a>
-
-      <nav className={styles.Navbar}>Navbar</nav>
+      <div className={styles.HeaderTwo}>
+        <a href="http://localhost:3000">
+          <img className={styles.Image} src={logo} alt="logo" />
+        </a>
+        <Navbar />
+      </div>
     </header>
   );
 };
