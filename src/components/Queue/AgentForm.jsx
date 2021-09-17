9977632ea3a4ba953cb66619227871c75d22599e
mathzb <Mathias.baungaard@gmail.com>
2021-09-18 01:30:17 +0200

    update(AgentForm.jsx) changed to use MUI

--- src/components/Queue/AgentForm.jsx
+++ src/components/Queue/AgentForm.jsx
@@ -1,35 +1,46 @@
-import React from "react";
-import styles from "../Table.module.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import styles from "../Queue/AgentForm.module.css";
 
 const AgentForm = ({ data }) => {
   return (
-    <>
-      <table className={styles.table}>
-        <tbody>
-          <tr className={styles.tableHeader}>
-            <th>Navn</th>
-            <th>Lokalnummer</th>
-            <th>Kald Besvaret</th>
-            <th>Gns. Samtaletid</th>
-            <th>Kald Omstillet</th>
-            <th>DND Tid (dagligt)</th>
-            <th>Pause Tid (dagligt)</th>
-          </tr>
-          {data &&
-            data.map((item) => (
-              <tr className={styles.tableHover} key={item.Agent}>
-                <td className={styles.tdCenter}>{item.Name}</td>
-                <td className={styles.tdCenter}>{item.Agent}</td>
-                <td className={styles.tdCenter}>{item.Calls}</td>
-                <td className={styles.tdCenter}>{item.AverageCalltime}</td>
-                <td className={styles.tdCenter}>{item.Transfers}</td>
-                <td className={styles.tdCenter}>{item.DND}</td>
-                <td className={styles.tdCenter}>{item.Pause}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </>
+    <div className={styles.table}>
+      {data && <h3>Du ser statistik på kø: {data[0].QueueName}</h3>}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="queue data table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Navn</TableCell>
+              <TableCell>Lokalnummer</TableCell>
+              <TableCell>Kald Besvaret</TableCell>
+              <TableCell>Gns. Samtaletid</TableCell>
+              <TableCell>Kald Omstillet</TableCell>
+              <TableCell>DND Tid (dagligt)</TableCell>
+              <TableCell>Pause Tid (dagligt)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data &&
+              data.map((item) => (
+                <TableRow className={styles.tableHover} key={item.Agent}>
+                  <TableCell>{item.Name}</TableCell>
+                  <TableCell>{item.Agent}</TableCell>
+                  <TableCell>{item.Calls}</TableCell>
+                  <TableCell>{item.AverageCalltime}</TableCell>
+                  <TableCell>{item.Transfers}</TableCell>
+                  <TableCell>{item.DND}</TableCell>
+                  <TableCell>{item.Pause}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
