4fa4595be60ccf1abf064f0e5c7d4adc4954a0c3
mathzb <mathias.jacobsen@ipnordic.dk>
2021-09-20 15:26:29 +0200

    new(Daily.jsx) added new component

---
+++ src/components/Queue/Daily.jsx
@@ -0,0 +1,52 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import styles from "./Daily.module.css";
+
+const Daily = ({ data }) => {
+  return (
+    <div className={styles.table}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="queue data table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Kønavn</TableCell>
+              <TableCell>Kønummer</TableCell>
+              <TableCell>Dato</TableCell>
+              <TableCell>Kald</TableCell>
+              <TableCell>Besvaret kald</TableCell>
+              <TableCell>Omstillet</TableCell>
+              <TableCell>Frafald</TableCell>
+              <TableCell>Gns. Samtaletid</TableCell>
+              <TableCell>Gns. Ventetid</TableCell>
+              <TableCell>Længste ventetid</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data &&
+              data.map((item) => (
+                <TableRow className={styles.tableHover} key={Math.random()}>
+                  <TableCell>{item.QueueName}</TableCell>
+                  <TableCell>{item.QueueExtension}</TableCell>
+                  <TableCell>{item.Date}</TableCell>
+                  <TableCell>{item.Calls}</TableCell>
+                  <TableCell>{item.AnsweredCalls}</TableCell>
+                  <TableCell>{item.Transfers}</TableCell>
+                  <TableCell>{item.Abondoned}</TableCell>
+                  <TableCell>{item.AverageCalltime}</TableCell>
+                  <TableCell>{item.AverageHoldtime}</TableCell>
+                  <TableCell>{item.MaxHoldtime}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Daily;
