6c759843bfa304c6c45519173faa0d9a3adaf51a
mathzb <mathias.jacobsen@ipnordic.dk>
2021-09-20 15:27:26 +0200

    update(PeriodForm.jsx) changed style location for component

--- src/components/Queue/PeriodForm.jsx
+++ src/components/Queue/PeriodForm.jsx
@@ -5,7 +5,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import styles from "../Table.module.css";
+import styles from "./PeriodForm.module.css";
 
 const Period = ({ data }) => {
   return (
