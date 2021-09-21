a74e29f71d43a2a08ec037aa4657e243fbed90e1
mathzb <Mathias.baungaard@gmail.com>
2021-09-21 22:43:47 +0200

    new components

---
+++ src/components/Routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Alert from "@mui/material/Alert";
+
+const Home = () => {
+  return (
+    <>
+      <Alert variant="standard" severity="info">
+        Denne side er under opbygning!
+      </Alert>
+    </>
+  );
+};
+
+export default Home;
