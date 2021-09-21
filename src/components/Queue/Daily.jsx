19cfa7b0c09b2110c7be649da298d835a3776e63
mathzb <Mathias.baungaard@gmail.com>
2021-09-21 19:39:15 +0200

    fix(Daily.jsx) table only renders when data is fetched

--- src/components/Queue/Daily.jsx
+++ src/components/Queue/Daily.jsx
@@ -5,46 +5,48 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import styles from "./Daily.module.css";
+import styles from "./Styles/Daily.module.css";
 
 const Daily = ({ apiData }) => {
   return (
     <div className={styles.table}>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="queue data table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Kønavn</TableCell>
-              <TableCell>Kønummer</TableCell>
-              <TableCell>Dato</TableCell>
-              <TableCell>Kald</TableCell>
-              <TableCell>Besvaret kald</TableCell>
-              <TableCell>Omstillet</TableCell>
-              <TableCell>Frafald</TableCell>
-              <TableCell>Gns. Samtaletid</TableCell>
-              <TableCell>Gns. Ventetid</TableCell>
-              <TableCell>Længste ventetid</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {apiData &&
-              apiData.map((item) => (
-                <TableRow className={styles.tableHover} key={Math.random()}>
-                  <TableCell>{item.QueueName}</TableCell>
-                  <TableCell>{item.QueueExtension}</TableCell>
-                  <TableCell>{item.Date}</TableCell>
-                  <TableCell>{item.Calls}</TableCell>
-                  <TableCell>{item.AnsweredCalls}</TableCell>
-                  <TableCell>{item.Transfers}</TableCell>
-                  <TableCell>{item.Abondoned}</TableCell>
-                  <TableCell>{item.AverageCalltime}</TableCell>
-                  <TableCell>{item.AverageHoldtime}</TableCell>
-                  <TableCell>{item.MaxHoldtime}</TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {apiData && (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="queue data table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Kønavn</TableCell>
+                <TableCell>Kønummer</TableCell>
+                <TableCell>Dato</TableCell>
+                <TableCell>Kald</TableCell>
+                <TableCell>Besvaret kald</TableCell>
+                <TableCell>Omstillet</TableCell>
+                <TableCell>Frafald</TableCell>
+                <TableCell>Gns. Samtaletid</TableCell>
+                <TableCell>Gns. Ventetid</TableCell>
+                <TableCell>Længste ventetid</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {apiData &&
+                apiData.map((item) => (
+                  <TableRow className={styles.tableHover} key={Math.random()}>
+                    <TableCell>{item.QueueName}</TableCell>
+                    <TableCell>{item.QueueExtension}</TableCell>
+                    <TableCell>{item.Date}</TableCell>
+                    <TableCell>{item.Calls}</TableCell>
+                    <TableCell>{item.AnsweredCalls}</TableCell>
+                    <TableCell>{item.Transfers}</TableCell>
+                    <TableCell>{item.Abondoned}</TableCell>
+                    <TableCell>{item.AverageCalltime}</TableCell>
+                    <TableCell>{item.AverageHoldtime}</TableCell>
+                    <TableCell>{item.MaxHoldtime}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </div>
   );
 };
