61a5e782d6a71d860d13565d4b258b5766ca4008
mathzb <Mathias.baungaard@gmail.com>
2021-09-21 22:44:13 +0200

    added router support

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import Header from "./components/Header";
-import Content from "./components/Content";
 import styles from "./App.module.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Routes/Home";
+import Content from "./components/Content";
+import Login from "./components/Routes/Login";
+import AgentDetails from "./components/Queue/AgentDetails";
 import "@fontsource/roboto/400.css";
 
 function App() {
   return (
     <div className={styles.Container}>
-      <Header />
-      <h1>ipnordic Statistik</h1>
-      <Content />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/statistik" component={Content} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/statistik/queuedetails/:id" component={AgentDetails} />
+        </Switch>
+      </Router>
     </div>
   );
 }
