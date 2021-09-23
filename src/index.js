d07d7aa907a4975ab9b046853905baf08a11ab3b
mathzb <Mathias.baungaard@gmail.com>
2021-09-23 22:44:39 +0200

    feat(index.js) added custom context from the Context API

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { CustomContextProvider } from "./Context/CustomContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CustomContextProvider>
+      <App />
+    </CustomContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
