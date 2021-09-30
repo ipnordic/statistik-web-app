bf57fc31bb9aea7be6dbf1664724794029542bcd
mathzb <mathias.baungaard@gmail.com>
2021-09-30 14:59:17 +0200

    added company state

--- src/hooks/useFetchAPI.jsx
+++ src/hooks/useFetchAPI.jsx
@@ -13,8 +13,25 @@ const useFetchAPI = () => {
     setError,
     userEmail,
     userPassword,
+    company,
   } = useContext(CustomContext);
 
+  // function setCharAt(str, index, chr) {
+  //   if (index > str.length - 1) return str;
+  //   return str.substring(0, index) + chr + str.substring(index + 1);
+  // }
+
+  // String.prototype.setCharAt = setCharAt;
+  // const toNumber = (number) => {
+  //   let sliceNum = number.slice(8, 10);
+  //   let convertToNumber = Number(sliceNum);
+  //   let increaseNumByOne = convertToNumber + 1;
+  //   const convertToString = increaseNumByOne.toString();
+
+  //   const replaceNum = number.setCharAt(number, 8, convertToString);
+  //   console.log(replaceNum);
+  // };
+  // toNumber(startDate);
   const fetchData = async () => {
     const API_URL = `https://api-prod01.ipnordic.dk/api/Statistics/Queue`;
     const options = {
@@ -27,7 +44,7 @@ const useFetchAPI = () => {
     try {
       setLoading(true);
       const response = await axios(
-        `${API_URL}/v2/${apiStatistics}?startDate=${startDate}&endDate=${endDate}&queue=${queueNumber}`,
+        `${API_URL}/v2/${apiStatistics}?startDate=${startDate}&endDate=${endDate}&company=${company}&queue=${queueNumber}`,
         options
       );
       setLoading(null);
