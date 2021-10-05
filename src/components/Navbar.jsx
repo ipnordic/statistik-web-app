e86c8bdf64cd5ad7f607bc4924d91ad120445c01
mathzb <mathias.baungaard@gmail.com>
2021-10-05 14:52:23 +0200

    hide Statistik in navbar when logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import styles from "./Header.module.css";
+import CustomContext from "../Context/CustomContext";
 
 const Navbar = () => {
+  const { isLoggedIn } = useContext(CustomContext);
   return (
     <nav>
       <ul className={styles.Navbar}>
         <li>
           <Link to="/">
-            <Button variant="contained">Hjem</Button>
+            <Button variant="contained">Forside</Button>
           </Link>
         </li>
-        {/* <li>
-          <Link to="/login">
-            <Button variant="contained">Login</Button>
-          </Link>
-        </li> */}
-        <li>
-          <Link to="/statistik">
-            <Button variant="contained">Statistik</Button>
-          </Link>
-        </li>
+        {!isLoggedIn ? (
+          <li>
+            <Link to="/statistik">
+              <Button variant="contained">Statistik</Button>
+            </Link>
+          </li>
+        ) : (
+          ""
+        )}
       </ul>
     </nav>
   );
