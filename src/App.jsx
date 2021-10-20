8cce78181dce45eed4ffa04abc23f15b9b8d68e3
mathzb <mathias.baungaard@gmail.com>
2021-10-20 11:15:35 +0200

    added logrocket identify

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,15 @@ import AgentDetails from "./components/Routes/AgentDetails";
 import Footer from "./components/Footer";
 import "semantic-ui-css/semantic.min.css";
 import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
+
 LogRocket.init("xnegd5/ipnordic-statistik");
+setupLogRocketReact(LogRocket);
+
+LogRocket.identify("0579fd1a-5e42-468c-a5df-73d2f2cadf4e", {
+  name: "Mathias Jacobsen",
+  email: "mathias.jacobsen@ipnordic.dk",
+});
 
 function App() {
   return (
