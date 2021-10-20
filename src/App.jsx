99ba42449215a9794cfeddb1f891b38fc35090a4
mathzb <mathias.baungaard@gmail.com>
2021-10-20 14:19:12 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Header from "./components/Header";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Routes/Home";
 import Content from "./components/Content";
-import Login from "./components/Routes/Login";
 import AgentDetails from "./components/Routes/AgentDetails";
 import Footer from "./components/Footer";
 import "semantic-ui-css/semantic.min.css";
@@ -25,9 +23,7 @@ function App() {
         <Header />
 
         <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/statistik" component={Content} exact />
-          <Route path="/login" component={Login} />
+          <Route path="/" component={Content} exact />
           <Route path="/statistik/detaljer/:queueId" component={AgentDetails} />
         </Switch>
       </Router>
