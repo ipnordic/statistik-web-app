6b9e76da24812bcf86186572202f357b2973462e
mathzb <mathias.baungaard@gmail.com>
2021-10-20 15:27:20 +0200

    changed class names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,16 @@ import { FaFacebook, FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className={styles.Footer}>
-      <div className={styles.FooterLeft}>
+      <div className={styles.Left}>
         <img src={logo} alt="logo" />
-        <span className={styles.FooterText}>&copy; 2021 ipnordic A/S</span>
+        <span className={styles.Text}>&copy; 2021 ipnordic A/S</span>
       </div>
-      <div className={styles.FooterRight}>
-        <a
-          className={styles.FooterRightSocialFB}
-          href="https://www.facebook.com/Ipnordic"
-        >
+      <div className={styles.Right}>
+        <a className={styles.SocialFB} href="https://www.facebook.com/Ipnordic">
           <FaFacebook />
         </a>
         <a
-          className={styles.FooterRightSocialLI}
+          className={styles.SocialLI}
           href="https://www.linkedin.com/company/ipnordic-a-s/"
         >
           <FaLinkedin />
