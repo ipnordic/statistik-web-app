5487d684a24b7c25882f6ef4e3f14c060fe121d0
mathzb <mathias.baungaard@gmail.com>
2021-10-22 13:27:20 +0200

    moved Header out of div

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,19 @@ import Content from "./components/Content";
 import AgentDetails from "./components/Routes/AgentDetails";
 import Footer from "./components/Footer";
 import "semantic-ui-css/semantic.min.css";
-import LogRocket from "logrocket";
-import setupLogRocketReact from "logrocket-react";
-
-LogRocket.init("xnegd5/ipnordic-statistik");
-setupLogRocketReact(LogRocket);
-
-LogRocket.identify("0579fd1a-5e42-468c-a5df-73d2f2cadf4e", {
-  name: "Mathias Jacobsen",
-  email: "mathias.jacobsen@ipnordic.dk",
-});
 
 function App() {
   return (
-    <div className={styles.Container}>
-      <Router>
-        <Header />
-
+    <Router>
+      <Header />
+      <div className={styles.Container}>
         <Switch>
           <Route path="/" component={Content} exact />
           <Route path="/statistik/detaljer/:queueId" component={AgentDetails} />
         </Switch>
-      </Router>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
