b3625728b70c1edf831d8a6de67493e4291bc8de
mathzb <mathias.baungaard@gmail.com>
2021-10-25 14:39:55 +0200

    added help button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from "react";
 import styles from "./Header.module.css";
 import CustomContext from "../Context/CustomContext";
 import Logout from "./Logout";
+import Help from "./Help";
 
 const Navbar = () => {
   const { isLoggedIn } = useContext(CustomContext);
   return (
     <nav>
       <ul className={styles.Navbar}>
+        <li>
+          <Help />
+        </li>
         {isLoggedIn ? (
           <li>
             <Logout />
