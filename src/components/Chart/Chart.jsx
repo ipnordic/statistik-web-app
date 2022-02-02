a38e2e4b0d94cd52a8debfa7a1fb443a644da95a
Mathias <Mathias.baungaard@gmail.com>
2022-02-02 15:01:07 +0100

    changed chart to show answeredcalls

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -16,7 +16,7 @@ const Chart = () => {
           datasets: [
             {
               label: "Besvaret",
-              data: apiData?.map((data) => data.Calls),
+              data: apiData?.map((data) => data.AnsweredCalls),
               backgroundColor: "rgba(54, 162, 235, 0.2)",
               borderColor: "rgba(54, 162, 235, 1)",
               borderWidth: 1,
