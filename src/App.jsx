8008cb94de5eb2eb600535ae45805e76d52f2fd4
Mathias <Mathias.baungaard@gmail.com>
2022-02-04 15:23:58 +0100

    redirect site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import Header from "./components/Header/Header";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import Content from "./components/Content";
-import AgentDetails from "./components/AgentDetails/AgentDetails";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import "semantic-ui-css/semantic.min.css";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 
 const AppContainer = styled.main`
   display: flex;
@@ -19,16 +14,22 @@ const AppContainer = styled.main`
 `;
 
 function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push("https://ipnordic.jcobsn.dev/");
+  }, [history]);
+
   return (
     <Router>
       <Header />
       <AppContainer>
         <Switch>
-          <Route path="/" component={Content} exact />
+          {/* <Route path="/" component={Content} exact />
           <Route path="/statistik/detaljer/:queueId" component={AgentDetails} />
           <Route path="*">
             <Redirect to="/" />
-          </Route>
+          </Route> */}
         </Switch>
       </AppContainer>
       <Footer />
